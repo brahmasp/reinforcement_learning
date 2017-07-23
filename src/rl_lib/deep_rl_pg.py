ba43e7e26c28f1283e0c880379aab13572064919
Brahma S. Pavse <brahmasp@utexas.edu>
2017-07-23 10:35:54 -0700

    initing things

--- src/rl_lib/deep_rl_pg.py
+++ src/rl_lib/deep_rl_pg.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+from math import sqrt
 
 """
 Using deep learning and policy gradients on Gym's game of Pong
@@ -21,7 +22,7 @@ of all the correct decisions given an input frame. Since we do not know
 the correct answer we take the log probs of all the actions we take till
 the end of the game, and at the end multiply those log probs with a scalar
 (+1 if we won, -1 if we lost). Accordingly maximizing this total sum will
-encourage good actions and discourage bad actions.
+encourage good actions and discourage bad actions. Perform gradient ascent.
 
 """
 
@@ -31,7 +32,8 @@ General procedure:
 - Preprocess the frame - resizing it to something smaller
 - Feed this into a 2 layer neural network
 - Spits out the probability of going UP
--  
+- After an episode (reaching 21 points), compute gradients
+- after 10 episodes (minibatch SGD) sum the gradients and move in gradient direction
 
 """
 
@@ -39,6 +41,43 @@ class PongAI(object):
 
 	def __init__(self):
 		self.env = gym.make('Pong-v0')
+		self.image_dim = 80 * 80 # pre processed image size
+		self.num_hidden_layer = 200 # number of neurons in single hidden layer
+		self.batch_size = 10 # mini batch for stochastic gradient descent
+		self.gamma = 0.99 # discount factor for reward function
+		self.decay_rate = 0.99 # RMSprop decay rate
+		self.learning_rate = 0.001 # alpha - during gradient descent
+
+		# Xavier inits of weights
+		# Divide by image_dim because 
+		# Xavier init worries about weights going into a single neuron
+		# here 6400 go into a neuron, if more weights each has lesser weight
+		# if less weights (< 6400) then each has higher weights
+		# similar you do that for each of the nerons in the hidden layer
+		# 200 * 6400
+		self.w1 = np.random.randn(self.num_hidden_layer, self.image_dim) * sqrt(2.0/self.image_dim)
+		
+		# weights for second layer
+		# 200 * 1
+		self.w2 = np.random.randn(self.num_hidden_layer, 1) * sqrt(2.0/self.num_hidden_layer)
+
+		# RMSprop inits. Used for optimizing gradient descent (http://ruder.io/optimizing-gradient-descent/index.html#rmsprop)
+		
+
+
+
+	def sigmoid(x):
+		return 1.0 / (1.0 + np.exp(-x))
+
+	# prepro image passed in is a 1 * 6400 unrolled image
+	def feedforward(self, prepro_image):
+		h = np.dot(prepro_image, self.w1)
+		h[h <= 0] = 0 # ReLU non linear function
+		# h is a 1 * 200 vector - output from each neuron
+		# 1 * 200 * 200 * 1 = 1 element
+		p = np.dot(h, self.w2)
+		return sigmoid (p)
+
 
 	# Credit to Karpathy's pg-pong.py
 	def preprocess(self, image):
@@ -52,7 +91,24 @@ class PongAI(object):
 
 
 
+#agent = PongAI()
 
+def temp_sim():
+	env = gym.make('Pong-v0')
+
+	env.reset()
+
+	while True:
+		env.render()
+
+		state, reward, done, _ = env.step(env.action_space.sample())
+		if reward !=0:
+			print(reward);
+
+		if done:
+			break;
+
+#temp_sim()
 
 
 
